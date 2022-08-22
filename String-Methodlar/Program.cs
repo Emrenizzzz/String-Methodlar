dd306b7ca3c4121e3fef9ce5d8bc028b86308b7d
Yunus Emre Başer <yunusemrebaser6021@gmail.com>
2022-08-22 22:41:37 +0300

    Proje dosyası ekle.

---
+++ String-Methodlar/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Methodlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string degisken = "Dersimiz CSharp, Hoşgeldiniz!";
+            string degisken2 = "CSharp";
+
+            //Length
+            Console.WriteLine(degisken.Length);
+
+            //ToUpper ToLower
+            Console.WriteLine(degisken.ToLower());
+            Console.WriteLine(degisken.ToUpper());
+
+            //Concat
+            Console.WriteLine(String.Concat(degisken," Merhaba!")); //sonuna ekler
+
+            //Compare CompareTo
+            Console.WriteLine(degisken.CompareTo(degisken2)); //0 1 -1
+            Console.WriteLine(String.Compare(degisken,degisken2,true)); //büyük küçük harf duyarsız
+            Console.WriteLine(String.Compare(degisken,degisken2,false)); //büyük küçük harf duyarlı
+
+            //Contains
+            Console.WriteLine(degisken.Contains(degisken2));
+            Console.WriteLine(degisken.EndsWith("Hoşgeldiniz!"));
+            Console.WriteLine(degisken.StartsWith("Merhaba!"));
+
+            //IndexOf
+            Console.WriteLine(degisken.IndexOf("CS"));
+            Console.WriteLine(degisken.IndexOf("Zikriye"));
+
+            //Insert
+            Console.WriteLine(degisken.Insert(0,"Merhaba! "));
+
+            //PadLeft PadRight
+            Console.WriteLine(degisken + degisken2.PadLeft(30)); //degisken2 nin solundan 30 karaktere tamamla
+            Console.WriteLine(degisken + degisken2.PadRight(30,'*')); // ""      sağından "    "       * ile tamamla
+            Console.WriteLine(degisken.PadRight(50) + degisken2);
+            Console.WriteLine(degisken.PadRight(50,'-') + degisken2);
+
+            //Remove
+            Console.WriteLine(degisken.Remove(10)); //10'uncu indexten itibaren (10 dahil) kaldır/sil
+            Console.WriteLine(degisken.Remove(5,3)); //5'inci indexten itibaren (5 dahil) 3 karakter kaldır/sil
+            Console.WriteLine(degisken.Remove(0,1));
+
+
+
+            //Replace
+            Console.WriteLine(degisken.Replace("CSharp","C#"));
+            Console.WriteLine(degisken.Replace(' ', '*'));
+
+            //Split
+            Console.WriteLine(degisken.Split(' ')[1]);
+
+            //Substring
+            Console.WriteLine(degisken.Substring(4));
+            Console.WriteLine(degisken.Substring(4,6));
+
+        }
+    }
+}
---
+++ String-Methodlar/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("String-Methodlar")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("String-Methodlar")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("0feda858-7b58-4224-af3e-2a888f26b2c0")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
